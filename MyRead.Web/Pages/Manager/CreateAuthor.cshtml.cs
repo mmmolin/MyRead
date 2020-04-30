676ff82f8f7508df5ead8fc4c479bdef2d2764ab
Martin <mmolin@telia.com>
2020-04-30 22:20:01 +0200

    Add new author page

---
+++ MyRead.Web/Pages/Manager/CreateAuthor.cshtml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyRead.Core;
+using MyRead.Core.Models;
+using MyRead.Data;
+
+namespace MyRead.Web.Pages.Manager
+{
+    public class CreateAuthorModel : PageModel
+    {
+        private readonly IData<Author> authorData;
+        public CreateAuthorModel(IData<Author> authorData)
+        {
+            this.authorData = authorData;
+        }
+
+        [BindProperty]
+        public AuthorModel AuthorModel { get; set; } 
+
+        public void OnGet()
+        {
+            AuthorModel = new AuthorModel();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            // automapper here
+            var authorEntity = new Author
+            {
+                FirstName = AuthorModel.FirstName,
+                LastName = AuthorModel.LastName
+            };
+
+            authorData.Add(authorEntity);
+            await authorData.CommitAsync();
+
+            return RedirectToPage("./CreateBook");
+        }
+    }
+}
