093cda254d98643dd3d75df877d26bc955dee875
Martin <mmolin@telia.com>
2020-05-07 21:11:22 +0200

    Change entity location, removing obsolete using statements

--- MyRead.Web/Pages/Index.cshtml.cs
+++ MyRead.Web/Pages/Index.cshtml.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
-using MyRead.Core;
+using MyRead.Core.Entities;
 using MyRead.Data;
 
 namespace MyRead.Web.Pages
--- MyRead.Web/Pages/Manager/DeleteBook.cshtml.cs
+++ MyRead.Web/Pages/Manager/DeleteBook.cshtml.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using MyRead.Core;
+using MyRead.Core.Entities;
 using MyRead.Data;
 
 namespace MyRead.Web.Pages.Manager
