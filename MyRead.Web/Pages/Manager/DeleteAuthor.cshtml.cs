2a2a53532fb94216079e844d06224805c75eff80
Martin <mmolin@telia.com>
2020-05-21 21:10:40 +0200

    added picture upload

--- MyRead.Web/Pages/Manager/DeleteAuthor.cshtml.cs
+++ MyRead.Web/Pages/Manager/DeleteAuthor.cshtml.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
 using MyRead.Core.Entities;
 using MyRead.Data;
 
@@ -27,11 +23,7 @@ namespace MyRead.Web.Pages.Manager
         public async Task<IActionResult> OnPost(int authorId) // Cascade did not work, books not deleted.
         {
             var authorEntity = await authorData.GetByIdAsync(authorId);
-            //var authorEntity = authorData.GetAll()
-            //    .Where(x => x.AuthorID == authorId)
-            //    .Include(x => x.Books)
-            //    .FirstOrDefault();
-
+            
             if(authorEntity != null)
             {
                 DeleteNotification = $"Deleted {authorEntity.FirstName} {authorEntity.LastName} from database";
